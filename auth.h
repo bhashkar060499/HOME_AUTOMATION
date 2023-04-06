0b6fa9586b4da703369ac629034ee30569eec56b
bhashkar060499 <66238035+bhashkar060499@users.noreply.github.com>
2023-04-06 17:22:21 +0530

    Add files via upload

---
+++ auth.h
@@ -0,0 +1,5 @@
+const char* ssid = "vivo 1820";
+const char* password = "123456789";
+const char* serverHostname = "192.168.0.101";
+const String serverUsername = "bhashkar";
+const String serverPassword = "123456789";
